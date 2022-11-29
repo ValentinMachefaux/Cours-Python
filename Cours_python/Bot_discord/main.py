9e867e1919efda85d8f1e4de96427142736d185b
Valentin Machefaux <valentin.machefaux@gmail.com>
2022-11-29 16:16:45 +0100

    Try to add buttons

--- Cours_python/Bot_discord/main.py
+++ Cours_python/Bot_discord/main.py
@@ -2,11 +2,7 @@ import discord
 import os
 from dotenv import load_dotenv
 from discord.ext import commands
-
-intents = discord.Intents.default()
-intents.messages = True
-intents.members = True
-intents.typing = True
+from discord_ui import Button
 
 # Charge le fichier .env qui est dans le meme dossier
 load_dotenv()
@@ -14,16 +10,56 @@ load_dotenv()
 # Recup le token discord
 DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
 
-# recup l'objet client de l'api discord.py
-bot = discord.Client(intents)
+# creation d'un nouvel objet bot avec un prefix
+bot = commands.Bot(command_prefix="!",intents=discord.Intents.all())
+embed = discord.Embed
+user = discord.user
 
-#@bot.command()
+@bot.command()
+async def hello(ctx):
+  await ctx.channel.send("Greetings")
+
 @bot.event
 async def on_message(message):
+  #await message.delete()
+  await bot.process_commands(message)
 
-  if message.content == "hello":
-    await message.channel.send("Greetings !")
+  if message.content == "blip":
+    await message.channel.send("bloup")
 
+@bot.command()
+async def game(ctx):
+
+  emd = embed(
+    title=ctx.author,
+    description="Voulez vous jouer a un jeu ? ",
+    components=[[
+      Button( 
+          color="blue",
+          custom_id="button1",
+          label="Blue button",
+          emoji="🚀",
+      ),
+      Button(
+          #style=ButtonStyle.red,
+          custom_id="button2",
+          label="Red button",
+          disabled=True,
+          emoji="🐺",
+      ),
+      Button(
+          #style=ButtonStyle.green,
+          custom_id="button3",
+          label="Green button",
+          emoji="😄",
+      ),
+    ]]
+  )
+  await ctx.send(embed=emd)
+
+@bot.event
+async def on_ready():
+  print("The bot is ready")
 
 
 
